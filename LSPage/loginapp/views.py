337a74a0126409ad5e28bab27843e879726b3207
HuzCodeHub <157280416+HuzCodeHub@users.noreply.github.com>
2024-02-21 17:31:49 +0530

    Update views.py

--- LSPage/loginapp/views.py
+++ LSPage/loginapp/views.py
@@ -1,59 +1,58 @@
 from django.shortcuts import render, redirect
-from . forms import CreateUserForm, LoginForm
+from .forms import UserAdminCreationForm
+from . forms import LoginForm 
 from django.contrib.auth.decorators import login_required
-
-# - Authentication models and functions
-
 from django.contrib.auth.models import auth
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate
+from django.db import DatabaseError
+from django.contrib import messages
+
 
 def homepage(request):
 
     return render(request, 'loginapp/index.html')
+           
 
 def signupPage(request):
-     form = CreateUserForm()
+     try:
+      form = UserAdminCreationForm()
 
-     if request.method == "POST":
+      if request.method == "POST":
 
-        form = CreateUserForm(request.POST)
+        form = UserAdminCreationForm(request.POST)
 
         if form.is_valid():
-
-            form.save()
-
-            return redirect("loginPage")
-
+                      form.save()
+                      return redirect('loginPage')
+     except DatabaseError as e:
+                    messages.error(request,"Email is already taken.")
 
      context = {'registerform':form}
+     return render(request, 'loginapp/signupPage.html', context=context)  
 
-     return render(request, 'loginapp/signupPage.html', context=context)
 
 
 def loginPage(request):
     form = LoginForm()
 
     if request.method == 'POST':
-
         form = LoginForm(request, data=request.POST)
-
         if form.is_valid():
+            email = request.POST.get('email')
+            password = form.cleaned_data['password']
+            print(email , password , request)
 
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-
-            user = authenticate(request, username=username, password=password)
+            # Authenticate user using email instead of username
+            user = authenticate(request, email= email , password=password)
 
             if user is not None:
-
+                print('asda')
                 auth.login(request, user)
-
                 return redirect("dashboard")
 
-
-    context = {'loginform':form}
-
+    context = {'loginform': form}
     return render(request, 'loginapp/loginPage.html', context=context)
+
 
 def user_logout(request):
     auth.logout(request)
